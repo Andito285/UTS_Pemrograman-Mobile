b556b742a22c5212d1dd70b61a46779f7ef096d8
Andito285 <anditomaulana7@gmail.com>
2025-11-03 07:28:21 +0700

    perbaikan

--- lib/main.dart
+++ lib/main.dart
@@ -14,10 +14,34 @@ class MyApp extends StatelessWidget {
       title: 'Cinema App',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primarySwatch: Colors.blue,
         scaffoldBackgroundColor: Colors.black,
         textTheme: const TextTheme(
           bodyMedium: TextStyle(color: Colors.white),
+          titleSmall: TextStyle(color: Colors.white),
+          titleMedium: TextStyle(color: Colors.white),
+          titleLarge: TextStyle(color: Colors.white),
+          labelLarge: TextStyle(color: Colors.white),
+        ),
+        inputDecorationTheme: const InputDecorationTheme(
+          labelStyle: TextStyle(color: Colors.grey),
+          hintStyle: TextStyle(color: Colors.grey),
+          enabledBorder: OutlineInputBorder(
+            borderSide: BorderSide(color: Colors.grey),
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderSide: BorderSide(color: Colors.blue),
+          ),
+        ),
+        cardColor: Colors.grey[900],
+        dividerColor: Colors.grey[700],
+        primaryColor: const Color(0xFF2C3E50),
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Color(0xFF2C3E50),
+          foregroundColor: Colors.white,
+        ),
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: const Color(0xFF2C3E50),
+          brightness: Brightness.dark,
         ),
       ),
       home: const LoginScreen(),
